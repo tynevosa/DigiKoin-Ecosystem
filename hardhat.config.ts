341fd30ff8169e487279b91ca768a3de82b7794f
tynevosa <michael.edy623@gmail.com>
2025-03-31 19:01:15 +0000

    chore: configure dotenv for environment variables in Hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
